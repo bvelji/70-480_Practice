27a9946805b27339f2100f8b2b296af2d09110db
Brian <bvelji@gmail.com>
2015-09-09 09:20:06 -0400

    web socket server
    create with fleck, run .exe before using example page

--- JS/websockets.js
+++ JS/websockets.js
@@ -1,4 +1,35 @@
-$(function(){
+$(function() {
+
+  function log(msg) {
+    $('#log').append("<li>" + msg + "</li>");
+  }
+
+  var webSocket = window.WebSocket || window.MozWebSocket,
+    ws = new webSocket("ws://localhost:8181");
+
+  log('Attempting to connect to server...');
+
+  ws.open = function() {
+    log("Connection opened.");
+  };
+
+  ws.onclose = function() {
+    log('Connection closed.');
+  };
+
+  ws.onmessage = function(e) {
+    log('Server: ' + e.data);
+  };
+
+  ws.onerror = function(e) {
+    log('!Error: ' + e);
+  };
+
+  $("#messageBox").change(function() {
+    ws.send(this.value);
+    $("#messageBox").val("");
+  });
+
 
 
 });
---
+++ WebSocketServer/WebSocketServer/HelloServer.cs
@@ -0,0 +1,43 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebSocketServer
+{
+    public class HelloServer
+    {
+        public void Start()
+        {
+            List<IWebSocketConnection> sockets = new List<IWebSocketConnection>();
+            Fleck.WebSocketServer server = new Fleck.WebSocketServer("ws://127.0.0.1:8181");
+
+            server.Start(socket => 
+                {
+                    socket.OnOpen = () =>
+                        {
+                            Console.WriteLine("Connection open.");
+                            sockets.Add(socket);
+                        };
+                    socket.OnClose = () =>
+                        {
+                            Console.WriteLine("Connection closed.");
+                            sockets.Remove(socket);
+                        };
+                    socket.OnMessage = message =>
+                        {
+                            Console.WriteLine("Client says: " + message);
+                            sockets.ToList().ForEach(s => s.Send(" client says:" + message));
+                        };
+
+                });
+
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                sockets.ToList().ForEach(s => s.Send(input));
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
