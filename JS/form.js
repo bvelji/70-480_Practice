aa3525552e032b24cfaa4af1982175d61a430b47
Brian <bvelji@gmail.com>
2015-09-10 15:20:10 -0400

    start of forms

--- JS/form.js
+++ JS/form.js
@@ -3,4 +3,12 @@ Form handling
 */
 $(function(){
 
+  $("#serialize").click(function(){
+    $("#results").text($("form").serialize());
+  });
+
+
+  $("#resetResults").click(function(){
+    $("#results").text("");
+  });
 });
