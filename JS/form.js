bc370382261c00820553db3eb077c0e591209f25
Brian <bvelji@gmail.com>
2015-10-01 14:40:50 -0400

    forms encode/decode

--- JS/form.js
+++ JS/form.js
@@ -11,4 +11,24 @@ $(function(){
   $("#resetResults").click(function(){
     $("#results").text("");
   });
+
+  $("#encodeURI").click(function(){
+    var uri = $("#results").text();
+    $("#results").text(encodeURI(uri));
+  });
+
+  $("#encodeURIComponent").click(function(){
+    var uri = $("#results").text();
+    $("#results").text(encodeURIComponent(uri));
+  });
+
+  $("#decodeURI").click(function(){
+    var uri = $("#results").text();
+    $("#results").text(decodeURI(uri));
+  });
+
+  $("#decodeURIComponent").click(function(){
+    var uri = $("#results").text();
+    $("#results").text(decodeURIComponent(uri));
+  });
 });
